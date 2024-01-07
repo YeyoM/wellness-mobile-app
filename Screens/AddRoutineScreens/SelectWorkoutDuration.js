d56950fad02e0420a10cbb36272b2abb702ad1a1
YeyoM <yeyomoreno2003@hotmail.com>
2024-01-07 12:26:17 -0600

    feat: :art: Select workout duration slider completed

--- Screens/AddRoutineScreens/SelectWorkoutDuration.js
+++ Screens/AddRoutineScreens/SelectWorkoutDuration.js
@@ -13,16 +13,33 @@ import TopNavigationBar from "../../components/TopNavigationBar";
 export default function SelectWorkoutDuration({ navigation }) {
 
   const [ value, onChangeText ] = useState('3');
-  const [ realTime, setRealTime ] = useState('1h 30m');
+  const [ realTime, setRealTime ] = useState('1:15');
+  const [ type, setType ] = useState('Minutes-Hours');
 
-  const progress = useSharedValue(1.5);
+  const progress = useSharedValue(1.25);
   const min = useSharedValue(0.25);
   const max = useSharedValue(3);
 
   const valueToTime = (value) => {
     const hours = Math.floor(value);
     const minutes = Math.round((value - hours) * 60);
-    setRealTime(`${hours}h ${minutes}m`);
+    if (hours === 0) {
+      setRealTime(`${minutes}`);
+      setType('Minutes');
+    }
+    else if (minutes === 0) {
+      setRealTime(`${hours}`);
+      if (hours === 1) {
+        setType('Hour');
+      }
+      else {
+        setType('Hours');
+      }
+    }
+    else {
+      setRealTime(`${hours}:${minutes}`);
+      setType('Minutes-Hours');
+    }
   }
 
   return (
@@ -30,8 +47,12 @@ export default function SelectWorkoutDuration({ navigation }) {
       <TopNavigationBar navigation={navigation} actualScreen={'AI routine maker'} back={true} />
       <View style={styles.content}>
         <Text style={{ color: '#fff', fontSize: 28, fontWeight: 'bold', marginBottom: 20, textAlign: 'center' }}>How long do you like your workouts to last?</Text>
+        <View style={{ width: '80%', marginBottom: 20, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+          <Ionicons name="help-circle-outline" size={18} color="#50535B" />
+          <Text style={{ color: '#50535B', fontSize: 13, fontWeight: 'bold', textAlign: 'justify', marginLeft: 3 }}>Drag to adjust</Text>
+        </View>
         <View style={{ width: '80%', marginBottom: 40, height: 300, display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 20 }}>
-          <View style={{ transform: [ { rotate: "-90deg" } ], display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-between', height: 50, width: 240, marginLeft: -80 }}>
+          <View style={{ transform: [ { rotate: "-90deg" } ], display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-between', height: 0, width: 240, marginLeft: -110 }}>
             <Slider
               progress={progress}
               minimumValue={min}
@@ -44,7 +65,7 @@ export default function SelectWorkoutDuration({ navigation }) {
                 onChangeText(e);
                 valueToTime(e);
               }}
-              thumbWidth={50}
+              thumbWidth={60}
               hapticMode={HapticModeEnum.STEP}
               theme={{
                 disableMinTrackTintColor: '#157AFF',
@@ -58,16 +79,33 @@ export default function SelectWorkoutDuration({ navigation }) {
                 height: 10,
                 backgroundColor: '#fff',
               }}
-              renderBubble={(props) => {return}}
+              renderBubble={(props) => { return }}
               renderThumb={(props) => {
                 return (
-                  <Image source={require('../../assets/thumb.png')} style={{ width: 50, height: 50 }} />
+                  <Image source={require('../../assets/thumb.png')} style={{ width: 60, height: 60 }} />
                 )
               }}
               sliderHeight={10}
             />
           </View>
-          <Text style={{ color: '#fff', fontSize: 60, fontWeight: 'bold', marginLeft: -100, textAlign: 'justify', alignSelf: "flex-end"}}> {realTime}</Text>
+          <View style={{ marginLeft: -110, width: "100%", height: "100%", display: "flex", flexDirection: "column", justifyContent: "flex-end" }}>
+            {
+              type === 'Minutes-Hours' ?
+                (
+                  <>
+                    <Text style={{ color: '#fff', fontSize: 100, fontWeight: '800', fontStyle: "italic", textAlign: 'justify', alignSelf: "flex-end" }}> {realTime}</Text>
+                    <Text style={{ color: '#fff', fontSize: 20, fontWeight: 'bold', textAlign: 'justify', alignSelf: "flex-end" }}>Minutes</Text>
+                  </>
+                )
+                :
+                (
+                  <>
+                    <Text style={{ color: '#fff', fontSize: 160, fontWeight: '800', fontStyle: "italic", textAlign: 'justify', alignSelf: "flex-end" }}> {realTime}</Text>
+                    <Text style={{ color: '#fff', fontSize: 20, fontWeight: 'bold', textAlign: 'justify', alignSelf: "flex-end" }}>{type}</Text>
+                  </>
+                )
+            }
+          </View>
         </View>
         <Pressable onPress={() => navigation.navigate('Add Routine')} style={styles.btnContinue}>
           <Text style={styles.btnContinueText}>Continue</Text>
