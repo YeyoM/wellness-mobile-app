926088c6e8253e09f1f2100aeef840241ad5fa8a
YeyoM <yeyomoreno2003@hotmail.com>
2024-02-28 13:26:58 -0600

    chore: :sparkles: Modified Save Workout firebase function to use transactions, assuring data integrity

--- FirebaseFunctions/Workouts/SaveWorkout.js
+++ FirebaseFunctions/Workouts/SaveWorkout.js
@@ -1,12 +1,5 @@
 import { FIRESTORE, FIREBASE_AUTH } from "../../firebaseConfig.js";
-import {
-  updateDoc,
-  runTransaction,
-  doc,
-  getDoc,
-  addDoc,
-  collection,
-} from "firebase/firestore";
+import { runTransaction, doc, collection } from "firebase/firestore";
 
 /**
  * SaveWorkout
@@ -60,52 +53,71 @@ export default async function SaveWorkout({
     throw new Error("Total time is missing!");
   }
 
+  // Create references for the documents we are going to update/modify
+  const userRef = doc(FIRESTORE, "users", userId);
+  const newWorkoutRef = doc(collection(FIRESTORE, "workouts"));
+  const exerciseRefs = [];
+  for (let i = 0; i < workout.length; i++) {
+    const exerciseId = workout[i].exerciseId;
+    const exerciseRef = doc(FIRESTORE, "exercises", exerciseId);
+    exerciseRefs.push(exerciseRef);
+  }
+
+  if (!userRef) {
+    throw new Error("User does not exist!");
+  }
+
+  if (!exerciseRefs) {
+    throw new Error("Exercises do not exist!");
+  }
+
+  if (exerciseRefs.length !== workout.length) {
+    throw new Error("Some exercises do not exist!");
+  }
+
+  if (!newWorkoutRef) {
+    throw new Error("Error creating reference for new workout");
+  }
+
   try {
-    // the name of the collection is "workouts"
-    const workoutRef = await addDoc(collection(FIRESTORE, "workouts"), {
-      userId,
-      routineId,
-      dayId,
-      workout,
-      totalCalories,
-      totalWeight,
-      totalTime,
-    });
-    console.log("AFTER SAVING THE WORKOUT");
-    console.log("workoutRef.id: ", workoutRef.id);
-    // add the workout id to the user's workout history it is an array in the user's document
-    const userRef = doc(FIRESTORE, "users", userId);
     await runTransaction(FIRESTORE, async (transaction) => {
       const userDoc = await transaction.get(userRef);
-      if (!userDoc.exists()) {
-        throw "Document does not exist!";
+      const exercisesDocs = [];
+      for (let i = 0; i < exerciseRefs.length; i++) {
+        const exerciseDoc = await transaction.get(exerciseRefs[i]);
+        exercisesDocs.push(exerciseDoc);
       }
-      const userWorkouts = userDoc.data().workouts;
-      userWorkouts.push(workoutRef.id);
-      transaction.update(userRef, { workouts: userWorkouts });
-    });
-    console.log("AFTER ADDING THE WORKOUT TO THE USER'S WORKOUT HISTORY");
-
-    // Now, for each of the exercises in the workout, we need to add the
-    // mean weight the user used to the exercise's weight history
-    for (let i = 0; i < workout.length; i++) {
-      const exerciseId = workout[i].exerciseId;
-      const exerciseRef = doc(FIRESTORE, "exercises", exerciseId);
-      const exerciseDoc = await getDoc(exerciseRef);
-      const exerciseData = exerciseDoc.data();
-      const weightHistory = exerciseData.weightRecord;
-      const newWeightHistory = {
-        date: new Date(),
-        weight: workout[i].exerciseWeight,
-      };
-      weightHistory.push(newWeightHistory);
-      await updateDoc(exerciseRef, {
-        weightRecord: weightHistory,
+      // workout
+      transaction.set(newWorkoutRef, {
+        userId,
+        routineId,
+        dayId,
+        workout,
+        totalCalories,
+        totalWeight,
+        totalTime,
       });
-      console.log("AFTER ADDING THE WEIGHT TO THE EXERCISE'S WEIGHT HISTORY");
-    }
+      console.log("WORKOUT WRITTEN TO TRANSACTION");
+      const userWorkouts = userDoc.data().workouts;
+      userWorkouts.push(newWorkoutRef.id);
+      transaction.update(userRef, { workouts: userWorkouts });
+      console.log("USER WORKOUTS UPDATE WRITTEN TO TRANSACTION");
+      for (let i = 0; i < exerciseRefs.length; i++) {
+        const weightHistory = exercisesDocs[i].data().weightRecord;
+        const newWeightHistory = {
+          date: new Date(),
+          weight: workout[i].exerciseWeight,
+        };
+        weightHistory.push(newWeightHistory);
+        transaction.update(exerciseRefs[i], {
+          weightRecord: weightHistory,
+        });
+      }
+      console.log("EXERCISES WEIGHT HISTORY WRITTEN TO TRANSACTION");
+    });
+    console.log("Transaction executed correctly");
   } catch (error) {
-    console.error("Error adding document: ", error);
-    throw error;
+    console.error("Error running transaction: ", error);
+    throw new Error("Error running transaction: ", error);
   }
 }
