646b73146039d806359fbcc9e3140d29aa979c9c
YeyoM <yeyomoreno2003@hotmail.com>
2024-04-26 09:06:19 -0600

    feat: :sparkles: Deleted the accomplishments section from the home sreen

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,11 +1,9 @@
-import React, { useEffect, useState, useContext } from "react";
-import { View, Text, StyleSheet, Image, Alert } from "react-native";
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 const Tab = createMaterialTopTabNavigator();
 
 import Constants from "expo-constants";
-import { Ionicons } from "@expo/vector-icons";
-import * as Progress from "react-native-progress";
 
 import MyPlan from "../components/MyPlan";
 import Crowdmeter from "../components/Crowdmeter";
@@ -36,28 +34,12 @@ export default function Home({ navigation }) {
             }}
           />
         </View>
-        <View style={styles.accomplishments}>
-          <View>
-            <Text style={styles.accomplishmentsText}>
-              You have completed 3 workouts this week
-            </Text>
-            <Text style={styles.accomplishmentsText_}>
-              You're 75% closer to reaching your weekly goal
-            </Text>
-            <Progress.Bar
-              progress={0.75}
-              width={200}
-              height={4}
-              color={"#0496FF"}
-              unfilledColor={"#fff"}
-              borderWidth={0}
-              borderRadius={8}
-              style={styles.progressBar}
-            />
-          </View>
-          <Ionicons name="trophy" size={30} color="#0496FF" />
-        </View>
-        <View style={{ width: "100%", minHeight: 500 }}>
+        <View
+          style={{
+            width: "100%",
+            flex: 1,
+          }}
+        >
           <Tab.Navigator
             screenOptions={{
               tabBarActiveTintColor: "#0496FF",
@@ -84,15 +66,17 @@ export default function Home({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#0B0B0B",
+    backgroundColor: "#0b0b0b",
     alignItems: "center",
     paddingTop: Constants.statusBarHeight,
   },
 
   home: {
+    flex: 1,
     display: "flex",
     alignItems: "center",
     width: "100%",
+    backgroundColor: "#fff",
   },
 
   header: {
@@ -119,32 +103,5 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#0496FF",
     marginBottom: 20,
-  },
-
-  accomplishments: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: "90%",
-    backgroundColor: "#24262B",
-    padding: 20,
-    borderRadius: 14,
-    marginBottom: 20,
-    marginTop: 20,
-  },
-
-  accomplishmentsText: {
-    color: "#fff",
-    fontSize: 12,
-  },
-
-  accomplishmentsText_: {
-    color: "#fff",
-    fontSize: 10,
-  },
-
-  progressBar: {
-    marginTop: 15,
   },
 });
