7ceef26b2926b1f048ff1d8578fcddf28df51916
YeyoM <yeyomoreno2003@hotmail.com>
2024-04-29 16:50:21 -0600

    feat: :sparkles: One Rep Maxes showing correctly! (For new users)

--- components/CarouselRepsMaxes.js
+++ components/CarouselRepsMaxes.js
@@ -1,20 +1,28 @@
+import React, { useState, useEffect, useRef, useContext } from "react";
+import { Dimensions, ActivityIndicator, View } from "react-native";
+
 import Carousel from "react-native-reanimated-carousel";
-
-import { Dimensions } from "react-native";
-import React from "react";
-
 import CarouselItemReps from "./CarouselItemReps";
 
-export default function CarouselDays({ exercises }) {
-  const [loop, _setLoop] = React.useState(false);
-  const [allowedExercises, setAllowedExercises] = React.useState([]);
-  const r = React.useRef(null);
+import saveDefaultExercises from "../FirebaseFunctions/Exercises/saveDefaultExercises";
+
+import { AppContext } from "../context/AppContext";
+
+export default function CarouselDays() {
+  const { firebaseUser, exercises, updateExercises } = useContext(AppContext);
+
+  const [loop, _setLoop] = useState(false);
+  const [allowedExercises, setAllowedExercises] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const r = useRef(null);
 
   if (!exercises) {
     return null;
   }
 
-  React.useEffect(() => {
+  useEffect(() => {
+    setLoading(true);
     const allowed = [
       "Barbell Bench Press",
       "Barbell Deadlift",
@@ -28,39 +36,68 @@ export default function CarouselDays({ exercises }) {
         filteredExercises.push(exercise);
       }
     });
-    setAllowedExercises(filteredExercises);
-  }, [exercises]);
+    if (filteredExercises.length === 4) {
+      console.log("Already saved exercises!");
+      setAllowedExercises(filteredExercises);
+      setLoading(false);
+    } else {
+      console.log("Saving default exercises...");
+      // in case the user has not saved the exercises yet, add those default exercises automatically
+      saveDefaultExercises(firebaseUser.uid, exercises)
+        .then((newExercises) => {
+          setAllowedExercises(newExercises);
+          updateExercises([...exercises, ...newExercises]);
+          setLoading(false);
+        })
+        .catch((error) => {
+          // in case of error, just show the exercises that are already saved
+          setAllowedExercises(filteredExercises);
+          setLoading(false);
+          console.error(error);
+        });
+    }
+  }, []);
 
   return (
-    <Carousel
-      key={`${loop}`}
-      ref={r}
-      loop={false}
-      onSnapToItem={(index) => console.log(index)}
-      style={{
-        width: Dimensions.get("window").width,
-        height: 120,
-        marginBottom: 100,
-        marginTop: 30,
-        marginLeft: 20,
-      }}
-      width={120}
-      height={120}
-      data={allowedExercises}
-      renderItem={({ item, animationValue }) => {
-        return (
-          <CarouselItemReps
-            animationValue={animationValue}
-            label={item}
-            onPress={() =>
-              r.current?.scrollTo({
-                count: animationValue.value,
-                animated: true,
-              })
-            }
-          />
-        );
-      }}
-    />
+    <View>
+      {loading ? (
+        <ActivityIndicator size="large" color="black" />
+      ) : allowedExercises && allowedExercises.length > 0 ? (
+        <Carousel
+          key={`${loop}`}
+          ref={r}
+          loop={false}
+          onSnapToItem={(index) => console.log(index)}
+          style={{
+            width: Dimensions.get("window").width,
+            height: 120,
+            marginBottom: 100,
+            marginTop: 30,
+            marginLeft: 20,
+          }}
+          width={120}
+          height={120}
+          data={allowedExercises}
+          renderItem={({ item, animationValue }) => {
+            return (
+              <CarouselItemReps
+                animationValue={animationValue}
+                label={item}
+                onPress={() =>
+                  r.current?.scrollTo({
+                    count: animationValue.value,
+                    animated: true,
+                  })
+                }
+              />
+            );
+          }}
+        />
+      ) : allowedExercises && allowedExercises.length === 0 ? (
+        <View style={styles.container}>
+          <Text>No exercises found</Text>
+        </View>
+      ) : null}
+    </View>
   );
 }
